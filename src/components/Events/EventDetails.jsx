651bbb0f55a743a090b6a3d745b54aabdf85e13c
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-13 18:13:41 +0100

    Event Detail

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,25 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent } from "../../util/http.js";
 
 export default function EventDetails() {
+  const { id } = useParams();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["Event"],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+  });
+
+  if (isLoading) return <p>Data Loading</p>;
+
+  const { title, image, location, date, time, description } = data;
+
   return (
     <>
       <Outlet />
@@ -13,20 +30,22 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={() => mutate(id)}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${image}`} alt="" />
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {date}:{time}
+              </time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{description}</p>
           </div>
         </div>
       </article>
