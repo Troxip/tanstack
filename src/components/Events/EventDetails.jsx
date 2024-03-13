b0012ea43e2af52441792a0527b678e421017a67
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-13 20:07:44 +0100

    Event detail

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -3,12 +3,13 @@ import { Link, Outlet, useParams } from "react-router-dom";
 import Header from "../Header.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { deleteEvent, fetchEvent } from "../../util/http.js";
+import ErrorBlock from "./../UI/ErrorBlock";
 
 export default function EventDetails() {
   const { id } = useParams();
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["Event"],
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
@@ -16,19 +17,33 @@ export default function EventDetails() {
     mutationFn: deleteEvent,
   });
 
-  if (isLoading) return <p>Data Loading</p>;
+  let content;
 
-  const { title, image, location, date, time, description } = data;
+  if (isPending)
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
 
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title={"Failed to load event"}
+          message={
+            error.info?.message ||
+            "Failed to fetch event data, please try again later"
+          }
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const { title, image, location, date, time, description } = data;
+    content = (
+      <>
         <header>
           <h1>{title}</h1>
           <nav>
@@ -48,7 +63,19 @@ export default function EventDetails() {
             <p id="event-details-description">{description}</p>
           </div>
         </div>
-      </article>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">{content}</article>
     </>
   );
 }
