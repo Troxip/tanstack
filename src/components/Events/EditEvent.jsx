d4a45a94a773f71d64e138d8eff49f05898e682a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-14 23:20:39 +0100

    Optimistic Updates

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { fetchEvent, updateEvent } from "../../util/http.js";
+import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
 import LoadingIndicator from "./../UI/LoadingIndicator";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -18,10 +18,23 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", id] });
+      const previousEvent = queryClient.getQueryData(["events", id]);
+
+      queryClient.setQueryData(["events", id], newEvent);
+
+      return { previousEvent };
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(["events", id], context.previousEvent);
+    },
   });
 
   function handleSubmit(formData) {
-    mutate({ id, event: data });
+    mutate({ id: id, event: formData });
     navigate("../");
   }
 
